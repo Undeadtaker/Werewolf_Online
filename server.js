d86feaa2671214c3febf580e6d7c0e3296c0046f
MitraMitraMitra <62009716+MitraMitraMitra@users.noreply.github.com>
2020-12-15 15:53:11 +0100

    15:52 15/12/2020

--- server.js
+++ server.js
@@ -179,3 +179,35 @@ io.on('connection', (socket) => {
 });
 
 
+// DUMMY LIST OF PLAYERS
+listOfPlayersNames = ['Marwin','Abdulkader','Luc','JP','Vasilli','Akash','Filip','GoogleBurger','El Chupacabra','Wumpus'];
+listOfPlayers = [];
+var i = 0;
+var player;
+var id;
+for (i=0;i<listOfPlayersNames.length;i++){
+    id = generate();
+    player = new user(listOfPlayersNames[i],id);
+    listOfPlayers.push(player);
+}
+
+listOfPlayersIDs = [];
+for (i=0;i<listOfPlayers.length;i++){
+    listOfPlayersIDs.push(listOfPlayers[i].id);
+}
+
+
+listOfRoles = ['werewolf','werewolf','werewolf','villager','villager','villager','hunter','seer','witch','cupid'];
+
+function assignRoles(players,roles){
+    var index;
+	for (i=0;i<players.length;i++){
+        index = Math.floor(Math.random()*roles.length);
+        players[i].role = roles[index];
+        alert(players[i].name+" is "+roles[index]);
+        roles.splice(index,1);
+        
+    }
+}
+
+assignRoles(listOfPlayers,listOfRoles);
