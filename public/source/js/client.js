b989dd887b47bac501921dd06d126e7af3f6a8eb
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2020-12-16 01:44:23 +0100

    Update client.js

--- public/source/js/client.js
+++ public/source/js/client.js
@@ -1,29 +1,84 @@
 var socket = io.connect('http://localhost:4000');
+var create = document.getElementById('host-a-game');
+
+
+
+const room_card_container = document.getElementsByClassName("game-rooms-container-child")[0];
+
+//function to create game room cards
+// parameter name is the game room name
+function create_game_room_card(name, players_in_game, max_players) {
+
+    const card = document.createElement("div");
+    card.setAttribute('class', 'room-card');
+
+    const room_name = document.createElement("h2");
+    room_name.innerText = name;
+    card.appendChild(room_name);
+
+    const ctj_text = document.createElement("h5");
+    ctj_text.innerText = "Click to join!"
+    card.appendChild(ctj_text);
+
+    const player_in_room = document.createElement("p");
+    player_in_room.innerText = "Players: " + players_in_game + "/" + max_players;
+    player_in_room.setAttribute("class", "numOfPlayers");
+    card.appendChild(player_in_room);
+
+
+    card.classList.add("room-card");
+
+    room_card_container.appendChild(card);
+
+}
+
+
+// a function to update the current player in room
+// parameter num is of type string and should be of format "current player number / Max Player number"
+function update_player_num_in_room(card, num) {
+    card.getElementsByTagName("p")[0].innerText = num;
+}
+
+
+
+// Call redirection
+create.addEventListener('click', e => {
+	socket.emit('host')
+});
+
+// Redirect
+socket.on('host', function(data){
+    window.location.href = data;
+});
+
 
 // Sending a request every 1 second for the list update
 setInterval(function(){
-    socket.emit('getPlayers')
+     socket.emit('getRooms')
 },1000);
 
-socket.on('getPlayers', function(rooms){
-	var room1 = document.getElementsByClassName('numOfPlayers');
-	room1[0].innerHTML = 'number of players: ' + rooms + '/30';
-});
+socket.on('getRooms', function(rooms_li){
+    room_card_container.innerHTML = '';
+    for (i = 0; i < rooms_li.length; i++){
+        create_game_room_card(rooms_li[i].name, rooms_li[i].Players.length, rooms_li[i].max_players);
+        room_card_container.children[i].setAttribute("id", rooms_li[i].id);
+    }
 
-
-
-
-
-
-// Redirecting Players if they clicked the element
-var room = document.getElementsByClassName('room-card');
-console.log(room[0])
-room[0].addEventListener('click', e => {
-	socket.emit('room1');
+    // YES!!! This is what we need!!! 
+    // select a list of elements and send the id of the room to join ..
+    document.querySelectorAll('div.room-card').forEach(item => {
+        item.addEventListener('click', event => {
+            // adding room id to localstorage
+            var json = JSON.parse(localStorage.getItem('MyVariable'));
+            old_name = JSON.parse(localStorage.getItem('MyVariable'))['name'];
+            old_id = JSON.parse(localStorage.getItem('MyVariable'))['id'];
+            localStorage.clear();
+            localStorage.setItem("MyVariable", JSON.stringify({'name' : old_name, 'id' : old_id, 'room_id' : item.id}));    
+            socket.emit('roomRedirect');
+        })
+    })
 });
 
 socket.on('redirect', function(destination){
-	window.location.href = destination;
+    window.location.href = destination;
 });
-
-
